1df367d985df44d472f1f020e717cdea966a838d
Alexander Lyotov <sippie84@gmail.com>
2022-10-27 19:02:34 +0300

    feature/year turned to IntegerField

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -5,6 +5,6 @@ from core.models import Album, Artist, Track
 
 class AlbumSerializer(serializers.Serializer):
     name = serializers.CharField(required=True)
-    year = serializers.CharField(required=True)
+    year = serializers.IntegerField(required=True)
     artist = serializers.CharField(required=True)
     tracks = serializers.ListField(required=True)
