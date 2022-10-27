3b88091aee1167078beaacdfd280a8c7ac78ddd1
Alexander Lyotov <sippie84@gmail.com>
2022-10-27 13:35:37 +0300

    feature/basic urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include("core.urls")),
 ]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Music Albums Guide API",
+        default_version="v1",
+        description="Документация для приложения backend проекта Music Albums Guide",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
