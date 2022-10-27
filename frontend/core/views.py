83bf5cdada20c5378b724e4bbcd4fd71cae181a5
Alexander Lyotov <sippie84@gmail.com>
2022-10-27 23:00:57 +0300

    feature/AlbumListFormView w.i.p.

--- frontend/core/views.py
+++ frontend/core/views.py
@@ -1,3 +1,29 @@
+import logging
+from django.views import View
 from django.shortcuts import render
 
-# Create your views here.
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+class AlbumListFormView(View):
+    def get(self, request):
+        # user_form = UserForm(instance=user)
+        # context={'user_form': user_form, 'profile_id': profile_id}
+        
+        album_sorting = request.GET.get('album_sort_button')
+        if album_sorting:
+            logger.debug(album_sorting)
+        
+        artist_sorting = request.GET.get('artist_sort_button')
+        if artist_sorting:
+            logger.debug(artist_sorting)
+        albums = [
+            {
+                'name': 'Album 1',
+                'artist': {'name': 'Artist 1'},
+                'tracks': ['track1', 'track2', 'track3']
+            }
+        ]
+        return render(request, 'core/albums.html', context={'albums': albums})
+
