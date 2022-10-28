5554cf33ac25b7b8e4a0750eb915beb6a175758e
Alexander Lyotov <sippie84@gmail.com>
2022-10-28 21:40:46 +0300

    feature/view finalized

--- frontend/core/views.py
+++ frontend/core/views.py
@@ -1,64 +1,26 @@
 import logging
 from django.views import View
 from django.shortcuts import render
-
+from rest_framework.exceptions import APIException
+from core.services import get_albums
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 
-TEST_ALBUMS = [
-    {
-        'album': 'Album 1[1969]',
-        'name': 'Album 1',
-        'artist': {'name': 'Artist 1'},
-        'tracks': ['track1', 'track2', 'track3']
-    },
-    {
-        'album': 'Album 2[2015]',
-        'name': 'Album 2',
-        'artist': {'name': 'Artist 2'},
-        'tracks': ['track1', 'track2', 'track3']
-    },
-    {
-        'album': 'Album 3[1999]',
-        'name': 'Album 3',
-        'artist': {'name': 'Artist 3'},
-        'tracks': ['track1', 'track2', 'track3']
-    }
-]
-class AlbumListFormView(View):
+class AlbumTableView(View):
     def get(self, request):
-        # user_form = UserForm(instance=user)
-        # context={'user_form': user_form, 'profile_id': profile_id}
-        
-        album_sorting = request.GET.get('album_sort_button')
-        if album_sorting:
-            logger.debug(album_sorting)
-        
-        artist_sorting = request.GET.get('artist_sort_button')
-        if artist_sorting:
-            logger.debug(artist_sorting)
-
-        return render(request, 'core/albums.html', context={'albums': TEST_ALBUMS})
+        logger.debug(get_albums(sorting='artist'))
+        try:
+            return render(request, 'core/albums.html', context={'albums': get_albums(sorting='artist')})
+        except APIException as exc:
+            return render(request, 'core/backend_error.html', context={'exc': exc})
 
     def post(self, request):
-        # user_form = UserForm(instance=user)
-        # context={'user_form': user_form, 'profile_id': profile_id}
-        logger.debug('POST method:')
-        logger.debug(request.POST)
-        album_sorting = request.POST.get('sort_by_album')
-        if album_sorting:
-            logger.debug(album_sorting)
+        sorting = request.POST.get('sort')
+        if sorting:
+            logger.debug(sorting)
+        else:
+            sorting = 'artist'
         
-        artist_sorting = request.POST.get('sort_by_artist')
-        if artist_sorting:
-            logger.debug(artist_sorting)
-        albums = [
-            {
-                'name': 'Album 1',
-                'artist': {'name': 'Artist 1'},
-                'tracks': ['track1', 'track2', 'track3']
-            }
-        ]
-        return render(request, 'core/albums.html', context={'albums': TEST_ALBUMS})
+        return render(request, 'core/albums.html', context={'albums': get_albums(sorting=sorting)})
