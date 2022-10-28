d253bec5e87914fbe2d69e5f515fd593ec24c15d
Alexander Lyotov <sippie84@gmail.com>
2022-10-28 21:38:23 +0300

    feature/import backend url and port

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -126,3 +126,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BACKEND_URL = 'http://{}:{}'.format(os.environ['BACKEND_HOST'], os.environ['BACKEND_PORT'])
