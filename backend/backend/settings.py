358cf77e9d2b68623c871b10b94e72ebc8fef050
Alexander Lyotov <sippie84@gmail.com>
2022-10-29 16:56:24 +0300

    feature/backend settings fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("BACKEND_DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
