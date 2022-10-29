367fc8074a3baf9a50afa6a137e58d6d91e2ec31
Alexander Lyotov <sippie84@gmail.com>
2022-10-29 16:02:24 +0300

    feature/entrypoints - no nginx

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
-exec "$@"
+# exec "$@"
--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-python manage.py runserver
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
-exec "$@"
+# exec "$@"
