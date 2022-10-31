6ae22b10579e30588addc0335b65a05b3d863712
Alexander Lyotov <sippie84@gmail.com>
2022-10-31 11:03:18 +0300

    feature/backend entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
-python manage.py runserver 0.0.0.0:8000
-# exec "$@"
+
+exec "$@"
